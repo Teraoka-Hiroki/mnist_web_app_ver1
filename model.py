060c4d6163e1c525aaf27d288ac6f7cb9fc8b738
Teraoka-Hiroki <88216511+Teraoka-Hiroki@users.noreply.github.com>
2023-03-04 19:42:16 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -39,7 +39,7 @@ def predict(img):
     # モデルへの入力
     img = img.convert("L")
     img = img.resize((img_size, img_size))
-#    img = 255 - img
+    img = 255 - img
     normalize = transforms.Normalize((0.0), (1.0))  # 平均値を0、標準偏差を1に
     to_tensor = transforms.ToTensor()
     transform = transforms.Compose([to_tensor, normalize])
